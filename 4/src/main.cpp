17e4eec845a58dc19b988a72a94adcbe0300dc1a
Rafał Michałuszek <rmichaluszek@gmail.com>
2022-12-15 20:20:29 +0100

    Blank puzzle template

---
+++ 4/src/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+#include "DataLoader.h"
+
+int main() {
+
+    DataLoader dataLoader;
+    std::vector<std::string> data = dataLoader.GetData();
+
+    system("pause");
+    // waits for any key to close the program, just to see results
+
+    return 0;
+}
