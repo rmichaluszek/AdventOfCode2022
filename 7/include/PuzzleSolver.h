1e63361b05b126f51b6b38d4739530fcfe894546
Rafał Michałuszek <rmichaluszek@gmail.com>
2022-12-27 23:51:27 +0100

    Added folder and files creating in directories

--- 7/include/PuzzleSolver.h
+++ 7/include/PuzzleSolver.h
@@ -15,14 +15,36 @@ public:
         return 0;
     }
 
-    void addFile(std::string name, int size) {
+    void addFile(std::string name, int size,std::vector<std::string> pathToCurrentFolder) {
         std::pair<std::string, int> newFile(name,size);
         this->files.push_back(newFile);;
     }
     void addFolder(std::string name) {
-        Folder newFolder;
-        newFolder.folderName = name;
-        this->folders.push_back(newFolder);
+        bool folderDoesExist = false;
+        for (size_t i = 0; i < this->folders.size(); i++) {
+            if (name == this->folders[i].folderName) {
+                folderDoesExist = true;
+                std::cout << "Folder exists: " << name << std::endl;
+            }
+        }
+
+        if (!folderDoesExist) {
+            Folder newFolder;
+            newFolder.folderName = name;
+            this->folders.push_back(newFolder);
+
+            std::cout << "Created folder: " << name << " in folder " << this->folderName << std::endl;
+        }
+    }
+
+    Folder* getFolder(std::string name) {
+        Folder *folder;
+        for(size_t i=0;i<this->folders.size();i++) {
+            if(this->folders[i].folderName==name) {
+                folder = &this->folders[i];
+            }
+        }
+        return folder;
     }
 };
 
--- 7/src/PuzzleSolver.cpp
+++ 7/src/PuzzleSolver.cpp
@@ -2,6 +2,50 @@
 
 void PuzzleSolver::ParseData(std::vector <std::string> data) {
 
+    bool listing = false;
+    std::vector<std::string> currentFolder; // by having that, we can be in /c/d/e folder and go back any time
+
+    for(size_t i=0; i<data.size();i++) {
+
+        if(data[i][0]=='$') { // this is out command, so were creating folders and files based on later output
+            std::string command;
+            std::string argument;
+
+            int delimeterPos = data[i].find(' ',2); // only searching for second space
+            command = data[i].substr(2,2);
+
+            if(delimeterPos != std::string::npos) {
+                argument = data[i].substr(delimeterPos+1,data[i].length());
+            }
+
+            if(command=="cd" && argument=="/") {
+                currentFolder.clear();
+            } else if (command=="cd") {
+                currentFolder.push_back(argument);
+            } else if (command == "ls") {
+                listing = true;
+            }
+
+        } else {
+            int delimeterPos = data[i].find(' ');
+            std::string firstPart = data[i].substr(0,delimeterPos);
+            std::string secondPart = data[i].substr(delimeterPos+1,data[i].length()-delimeterPos);
+
+            std::vector<std::string> pathToFolder = currentFolder;
+
+            if(firstPart=="dir") {
+                std::cout << "dir " << secondPart << " // size:" << currentFolder.size() << std::endl;
+                Folder *parentFolder = &this->fileSystem;
+                while(pathToFolder.size()>0){
+                    parentFolder=parentFolder->getFolder(pathToFolder[0]);
+                    pathToFolder.erase(pathToFolder.begin());
+                }
+                parentFolder->addFolder(secondPart);
+            } else {
+                this->fileSystem.addFile(secondPart,stoi(firstPart),currentFolder);
+            }
+        }
+    }
 }
 
 int PuzzleSolver::SolvePart1() {
